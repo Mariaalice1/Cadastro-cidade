79dd521c746d28992f54355a7bfa18a8dc575de9
Mariaalice1 <85592927+Mariaalice1@users.noreply.github.com>
2021-09-07 11:53:04 -0300

    Update principal.py

--- principal.py
+++ principal.py
@@ -1,13 +1,28 @@
-from src.classes.Cidade import cidade
+from src.classes.Cidade import Cidade
+import json
 
 sair = False
+
+arquivo = open("./src/bd/bd.json", 'r') 
+lista_cidades = json.loads(arquivo.read())
+arquivo.closer()
+
 while sair == False:
-     print("repetindo")
 
-resposta = input("Deseja cadastrar outra cidade?")
+    nome_cidade = input("Digite o nome da cidade:")
+    populacao_cidade = int(input("Digite a população da cidade:"))
+    sigla_estado = input("Digite a sigla do estado:")
+    nome_estado = input("Digite o nome do estado")
 
-while resposta.upper() != "S" and resposta.upper() != "N":
-    print("A resposta deve ser N ou S")
-    resposta =  input('Deseja cadastrar outra cidade?')
+    uf = {"sigla": sigla_estado, "nome": nome_estado}
+    nova_cidade = Cidade(nome_cidade, populacao_cidade,uf)
+
+lista_cidades.append(nova_cidade)
+ 
+resposta =  input('Deseja cadastrar outra cidade?')
+   
 if resposta.upper () == "N":
-    sair = True
+         sair = True
+
+arquivo = open ("./src/bd/bd.json", 'w')
+arquivo.write(json.dumps(lista_cidades))
